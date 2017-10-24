5c290f522318126e11cb8e4dfd6bfc9a3ab047a3
Crista Cooper <aine3d@gmail.com>
2017-10-24 20:02:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,20 +69,16 @@ game.appendChild(rock);
   function moveRock() {
     // implement me!
     // (use the comments below to guide you!)
-    /**
-     * If a rock collides with the DODGER,
-     * we should call endGame()
-     */
+    rock.style.top = `${top += 2}px`;
 
-    /**
-     * Otherwise, if the rock hasn't reached the bottom of
-     * the GAME, we want to move it again.
-     */
+    	if (checkCollision(rock)) {
+        return endGame();
+      } else if (top < GAME_HEIGHT) {
+        window.requestAnimationFrame(moveRock);
+      } else {
+        rock.remove();
+      }
 
-    /**
-     * But if the rock *has* reached the bottom of the GAME,
-     * we should remove the rock from the DOM
-     */
   }
 
   // We should kick of the animation of the rock around here
